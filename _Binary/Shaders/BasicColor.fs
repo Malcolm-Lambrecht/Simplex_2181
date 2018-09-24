001dd4923188a363d581b7f6c8a7888aad3c4935
Malcolm Lambrecht <jml1769@rit.edu>
2018-09-24 09:29:10 -0400

    e04 complete

--- _Binary/Shaders/BasicColor.fs
+++ _Binary/Shaders/BasicColor.fs
@@ -3,7 +3,7 @@
 in vec3 Color;
 
 uniform vec3 SolidColor = vec3(-1,-1,-1);
-uniform bool DisplayComplementary;
+
 out vec4 Fragment;
 
 void main()
@@ -12,10 +12,6 @@ void main()
 	
 	if(SolidColor.r != -1.0 && SolidColor.g != -1.0 && SolidColor.b != -1.0)
 		Fragment = vec4(SolidColor, 1);
-	if(DisplayComplementary){
-		Fragment.x = 1.0f - Fragment.x;
-		Fragment.y = 1.0f - Fragment.y;
-		Fragment.z = 1.0f - Fragment.z;
-		}
+
 	return;
 }
